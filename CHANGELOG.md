1e6cba65d0078d889645d7ec8cdb3ded3eb39607
Israel Kliger <kligernator@gmail.com>
2026-03-31 00:55:19 +0300

    Fix deprecated runReadAction API warning for IntelliJ 2026.1+

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,11 @@ All notable changes to the "Copy with inline issues" plugin will be documented i
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.4/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.1.1]
+
+### Fixed
+- **Deprecated API migration** - Replaced deprecated `runReadAction(Function0)` with `ReadAction.compute()` in `ProblemDetectionService.runInspectionsOnRange()` to resolve compatibility warning with IntelliJ Platform 261+
+
 ## [1.1.0]
 
 ### Changed
--- src/main/kotlin/com/github/israelkli/intellijplugincopyfilewithproblems/services/ProblemDetectionService.kt
+++ src/main/kotlin/com/github/israelkli/intellijplugincopyfilewithproblems/services/ProblemDetectionService.kt
@@ -5,7 +5,7 @@ import com.intellij.codeInspection.InspectionManager
 import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.lang.annotation.HighlightSeverity
-import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.editor.impl.DocumentMarkupModel
 import com.intellij.profile.codeInspection.InspectionProjectProfileManager
 import com.intellij.psi.PsiDocumentManager
@@ -117,12 +117,8 @@ class ProblemDetectionService {
                         val inspectionManager = InspectionManager.getInstance(project)
                         
                         // Run inspection on the file with proper error handling
-                        val descriptors = runReadAction {
-                            try {
-                                inspectionTool.checkFile(psiFile, inspectionManager, false) ?: emptyArray()
-                            } catch (_: Exception) {
-                                emptyArray<ProblemDescriptor>()
-                            }
+                        val descriptors = ReadAction.compute<Array<ProblemDescriptor>, Exception> {
+                            inspectionTool.checkFile(psiFile, inspectionManager, false) ?: emptyArray()
                         }
                         
                         // Filter descriptors that are within our range
